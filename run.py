e480964d9366a8fd807e3aa5ae02d12981446df5
Sejung Kwak <kwak.sejung@gmail.com>
2022-05-12 22:52:14 +0100

    feat: Add draw condition

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ def display_board(board):
   for slot in board:
     if slot in board[:2] or slot in board[3:5] or slot in board[6:8]:
       print(f" {slot} |", end="")
-    if slot == 9 or slot == 6:
+    if slot == board[2] or slot == board[5]:
       print(f" {slot} ")
       print("-----------")
-    if slot == 3:
-      print(f" {slot} ")
+    if slot == board[8]:
+      print(f" {slot} \n")
 
 # Displays the information
 def dsiplay_greeting():
@@ -56,33 +56,6 @@ def player_ready(player):
       print("You have entered an invalid answer. "
         "Please enter Y or N.")
 
-# Win conditions
-def check_win(board):
-
-  if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] ==board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
-    return True
-  else:
-    pass
-    
-# Play again
-def play_again():
-  answer = 'A'
-  choice = ['Y', 'N']
-
-  while answer.upper() not in choice:
-    answer = input("Do you want to play again? "
-      "Please enter [Y]es / [N]o")
-    
-    if answer.upper() == 'Y':
-      board = [7, 8, 9, 4, 5, 6, 1, 2, 3]
-      dsiplay_greeting()
-      break
-    elif answer.upper() == 'N':
-      print("Thank you for playing :)")
-      break
-    else:
-      print("Please enter Y or N.")
-
 # Gets user input
 def player_move(board, player):
 
@@ -107,8 +80,14 @@ def player_move(board, player):
     if move == val:
       board[idx] = player
       gameover = check_win(board)
+      draw = check_draw(board)
       if gameover:
         print("Congratulations! You have won the game!")
+        player_move(board, player)
+        play_again()
+      elif draw:
+        print("Oh well, it is a draw xD")
+        player_move(board, player)
         play_again()
       else:
         if player == 'X':
@@ -117,6 +96,34 @@ def player_move(board, player):
           player = 'X'
         player_move(board, player)
 
-# Updates game board
+# Win conditions
+def check_win(board):
 
-# Displays result
+  if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] ==board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
+    return True
+
+# Checks if the game is a draw
+def check_draw(board):
+  if board.count('X') == 5 or board.count('O') == 5:
+    return True
+
+# Play again
+def play_again():
+  answer = 'A'
+  choice = ['Y', 'N']
+
+  while answer.upper() not in choice:
+    answer = input("Do you want to play again? "
+      "Please enter [Y]es / [N]o")
+    
+    if answer.upper() == 'Y':
+      board = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+      dsiplay_greeting()
+      break
+    elif answer.upper() == 'N':
+      print("Thank you for playing :)")
+      break
+    else:
+      print("Please enter Y or N.")
+
+dsiplay_greeting()
