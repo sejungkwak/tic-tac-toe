c97e67ac2523dd771c9667ee8ad0cad67734849c
Sejung Kwak <kwak.sejung@gmail.com>
2022-05-12 20:54:46 +0100

    feat: Add board

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
+# Game board
+board = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+def display_board(board):
+  
+  for slot in board:
+    if slot in board[:2] or slot in board[3:5] or slot in board[6:8]:
+      print(f" {slot} |", end="")
+    if slot == 9 or slot == 6:
+      print(f" {slot} ")
+      print("-----------")
+    if slot == 3:
+      print(f" {slot} ")
+
 # Displays the information
 def dsiplay_greeting():
   print("Welcome to Tic Tac Toe!")
+  display_board(board)
   game_setting()
 
 # Player 1's chracter
@@ -39,8 +53,6 @@ def player_ready():
     else:
       print("You have entered an invalid answer. "
         "Please enter Y or N.")
-
-# Game board
 
 # Win conditions
 
